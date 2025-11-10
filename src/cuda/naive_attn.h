5347ae207d857ceda4b7fa69956d3b987ea7e68c
Dolan Clahan <dolan.clahan@complex.com>
2025-11-09 22:53:29 -0500

    no semicolon

--- src/cuda/naive_attn.h
+++ src/cuda/naive_attn.h
@@ -1,7 +1,7 @@
 #ifndef NAIVE_ATTN_H
 #define NAIVE_ATTN_H
 
-// void naive_attention(const float* Q, const float* K, const float* V, float* O, int N, int M, int d);
 int naive_attention(const float*, const float*, const float*, float*, int, int, int);
+// void naive_attention(const float* Q, const float* K, const float* V, float* O, int N, int M, int d);
 
 #endif
