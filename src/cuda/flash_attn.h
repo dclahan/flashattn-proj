f55f2d901b75e3dc419d386c8363c4a0174194b0
Dolan Clahan <dolan.clahan@complex.com>
2025-11-09 22:56:16 -0500

    headers need param names

--- src/cuda/flash_attn.h
+++ src/cuda/flash_attn.h
@@ -1,7 +1,7 @@
 #ifndef FLASH_ATTN_H
 #define FLASH_ATTN_H
 
-float* flash_forward(float*, float*, float*, int, int , int, int);
-// float* flash_forward(float* Q, float* K, float* V, int B, int nh, int N, int d);
+float* flash_forward(float* Q, float* K, float* V, int B, int nh, int N, int d);
+// float* flash_forward(float*, float*, float*, int, int , int, int);
 
 #endif
