f02e75bcd2f6992e5d077adf8c8e2e214e9c54d1
Dolan Clahan <dolan.clahan@complex.com>
2025-11-09 22:50:06 -0500

    ifndefgaurds

--- src/cuda/flash_attn.h
+++ src/cuda/flash_attn.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef FLASH_ATTN_H
+#define FLASH_ATTN_H
 
 // float* flash_forward(float* Q, float* K, float* V, int B, int nh, int N, int d);
 float* flash_forward(float*, float*, float*, int, int , int, int)
+
+#endif
--- src/cuda/naive_attn.h
+++ src/cuda/naive_attn.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef NAIVE_ATTN_H
+#define NAIVE_ATTN_H
 
 // void naive_attention(const float* Q, const float* K, const float* V, float* O, int N, int M, int d);
 void naive_attention(const float*, const float*, const float*, float*, int, int, int);
+
+#endif
