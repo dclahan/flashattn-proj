e1aaf301cbb4e312f3bc438f29dcc5ba47ed5e87
Dolan Clahan <dolan.clahan@complex.com>
2025-11-09 22:42:37 -0500

    commit to git

--- src/cuda/naive_attn.h
+++ src/cuda/naive_attn.h
@@ -1,4 +1,4 @@
 #pragma once
 
-// void naive_attention(const float* Q, const float* K, const float* V, float* O, int N, int M, int d);
-void naive_attention(const float*, const float*, const float*, float*, int, int, int);
+void naive_attention(const float* Q, const float* K, const float* V, float* O, int N, int M, int d);
+// void naive_attention(const float*, const float*, const float*, float*, int, int, int);
