d701c52bb100381d67cfd5ef1c601c8675ba3441
Dolan Clahan <dolan.clahan@complex.com>
2025-11-15 00:17:57 -0500

    saving the day

--- src/cuda/flash_attn.cu
+++ src/cuda/flash_attn.cu
@@ -35,68 +35,72 @@ __global__ void flash_attn_forward_kernel(
     qkv_offset = (bx * gridDim.y * N * d) + (by * N * d);
     lm_offset  = (bx * gridDim.y * N)     + (by * N);
 
-    // sram for Q,K,V,S
-    extern __shared__ float sram[];
-    // above should be on SM shared memory
     tile_size = Bc * d;
-    Qi = sram;
-    Kj = &sram[tile_size];
-    Vj = &sram[tile_size * 2];
-    S  = &sram[tile_size * 3];
 
-    for (int j = 0; j < Tc; j++){
-        __syncthreads();
-        // load Kj, Vj into sram
-        for (x = 0; x < d; x++){
-            Kj[(tx * d) + x] = K[qkv_offset + (tile_size * j) + (tx * d) + x];
-            Vj[(tx * d) + x] = V[qkv_offset + (tile_size * j) + (tx * d) + x];
-        }
-        __syncthreads();
-        for (int i = 0; i < Tr; i++){
-            // load Qi to shared mem, li, mi to registers
-            for (x = 0; x < d; x++)
-                Qi[(tx * d) + x] = Q[qkv_offset + (tile_size * i) + (tx * d) + x];
-            row_m_prev = m[lm_offset + (Br * i) + tx];
-            row_l_prev = l[lm_offset + (Br * i) + tx];
+    // check to see if thread is reading outside of QKV matrices
+    if (qkv_offset + (tile_size * (Tc-1)) + (tx * d) < gridDim.x * gridDim.y * N * d){
 
-            // compute Sij = softmax_scale * Qi * Kj^T
-            // m_tilde_ij = rowmax(Sij)
-            row_m = -INFINITY;
-            for (y = 0; y < Bc; y++){
-                sum = 0;
-                for (x = 0; x < d; x++)
-                    sum += Qi[(tx * d) + x] * Kj[(y * d) + x];
-                sum *= softmax_scale;
-                S[(Bc * tx) + y] = sum;
-                if (sum > row_m) row_m = sum;
-            }
+        // sram for Q,K,V,S
+        extern __shared__ float sram[];
+        Qi = sram;
+        Kj = &sram[tile_size];
+        Vj = &sram[tile_size * 2];
+        S  = &sram[tile_size * 3];
 
-            // 12. compute
-            // P_tilde_ij = exp(Sij_masked - m_tilde_ij)
-            // l_tilde_ij = rowsum(P_tilde_ij)
-            row_l = 0;
-            for (y = 0; y < Bc; y++) {
-                S[(Bc * tx) + y] = __expf(S[(Bc * tx) + y] - row_m);
-                row_l += S[(Bc * tx) + y];
-            }
-
-            // 13. compute mi_new, li_new
-            row_m_new = max(row_m_prev, row_m);
-            row_l_new = (__expf(row_m_prev - row_m_new) * row_l_prev) + (__expf(row_m - row_m_new) * row_l);
-
-            // 15. Write Oi to global mem
+        for (int j = 0; j < Tc; j++){
+            __syncthreads();
+            // load Kj, Vj into sram
             for (x = 0; x < d; x++){
-                pv = 0;
-                for (y = 0; y < Bc; y++)
-                    pv += S[(Bc * tx) + y] * Vj[(y * d) + x];
-                O[qkv_offset + (tile_size * i) + (tx * d) + x] = (1 / row_l_new) \
-                    * ((row_l_prev * __expf(row_m_prev - row_m_new) * O[qkv_offset + (tile_size * i) + (tx * d) + x]) \
-                    + (__expf(row_m - row_m_new) * pv));
+                Kj[(tx * d) + x] = K[qkv_offset + (tile_size * j) + (tx * d) + x];
+                Vj[(tx * d) + x] = V[qkv_offset + (tile_size * j) + (tx * d) + x];
             }
+            __syncthreads();
+            for (int i = 0; i < Tr; i++){
+                // load Qi to shared mem, li, mi to registers
+                for (x = 0; x < d; x++)
+                    Qi[(tx * d) + x] = Q[qkv_offset + (tile_size * i) + (tx * d) + x];
+                row_m_prev = m[lm_offset + (Br * i) + tx];
+                row_l_prev = l[lm_offset + (Br * i) + tx];
 
-            // 16. Write li <- li_new, mi <- mi_new to global mem (HBM)
-            m[lm_offset + (Br * i) + tx] = row_m_new;
-            l[lm_offset + (Br * i) + tx] = row_l_new;
+                // compute Sij = softmax_scale * Qi * Kj^T
+                // m_tilde_ij = rowmax(Sij)
+                row_m = -INFINITY;
+                for (y = 0; y < Bc; y++){
+                    sum = 0;
+                    for (x = 0; x < d; x++)
+                        sum += Qi[(tx * d) + x] * Kj[(y * d) + x];
+                    sum *= softmax_scale;
+                    S[(Bc * tx) + y] = sum;
+                    if (sum > row_m) row_m = sum;
+                }
+
+                // 12. compute
+                // P_tilde_ij = exp(Sij_masked - m_tilde_ij)
+                // l_tilde_ij = rowsum(P_tilde_ij)
+                row_l = 0;
+                for (y = 0; y < Bc; y++) {
+                    S[(Bc * tx) + y] = __expf(S[(Bc * tx) + y] - row_m);
+                    row_l += S[(Bc * tx) + y];
+                }
+
+                // 13. compute mi_new, li_new
+                row_m_new = max(row_m_prev, row_m);
+                row_l_new = (__expf(row_m_prev - row_m_new) * row_l_prev) + (__expf(row_m - row_m_new) * row_l);
+
+                // 15. Write Oi to global mem
+                for (x = 0; x < d; x++){
+                    pv = 0;
+                    for (y = 0; y < Bc; y++)
+                        pv += S[(Bc * tx) + y] * Vj[(y * d) + x];
+                    O[qkv_offset + (tile_size * i) + (tx * d) + x] = (1 / row_l_new) \
+                        * ((row_l_prev * __expf(row_m_prev - row_m_new) * O[qkv_offset + (tile_size * i) + (tx * d) + x]) \
+                        + (__expf(row_m - row_m_new) * pv));
+                }
+
+                // 16. Write li <- li_new, mi <- mi_new to global mem (HBM)
+                m[lm_offset + (Br * i) + tx] = row_m_new;
+                l[lm_offset + (Br * i) + tx] = row_l_new;
+            }
         }
     }
 }
@@ -142,7 +146,7 @@ float* flash_forward(
     delete[] m_host;
 
     // Calculate SRAM size needed per block
-    const int sram_size = (4 * Bc * d * sizeof(float)); // + (Bc * Br * sizeof(float));
+    const int sram_size = (3 * Bc * d * sizeof(float)) + (Bc * Br * sizeof(float));
     int max_sram_size;
     cudaDeviceGetAttribute(&max_sram_size, cudaDevAttrMaxSharedMemoryPerBlock, 0);
     printf("Max shared memory: %d, requested shared memory: %d\n", max_sram_size, sram_size);
@@ -151,7 +155,7 @@ float* flash_forward(
     dim3 grid_dim(B, nh);  // batch_size x num_heads
     dim3 block_dim(Bc);    // Bc threads per block
 
-    printf("grid dim = %d x %d, block dim = %d\n", B, nh,Bc);
+    printf("grid dim = %d x %d, block dim = %d\n", B, nh, Bc);
 
     // Launch kernel
     // printf("Launching Forward Kernel\n");
