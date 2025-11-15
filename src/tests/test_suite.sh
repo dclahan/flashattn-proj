1cde4b81ffdcfce0b76582ae736e7a37efafec50
Dolan Clahan <dolan.clahan@complex.com>
2025-11-15 02:55:46 -0500

    add test suite, cuda2,5 done

---
+++ src/tests/test_suite.sh
@@ -0,0 +1,21 @@
+# Test suite
+
+./benchmark_attn 8 8 64 64
+./benchmark_attn 16 8 64 64
+./benchmark_attn 32 8 64 64
+
+./benchmark_attn 8 12 64 64
+./benchmark_attn 16 12 64 64
+./benchmark_attn 32 12 64 64
+
+./benchmark_attn 8 16 64 64
+./benchmark_attn 16 16 64 64
+./benchmark_attn 32 16 64 64
+
+./benchmark_attn 8 12 128 64
+./benchmark_attn 16 12 128 64
+
+./benchmark_attn 8 8 256 64
+./benchmark_attn 8 12 256 64
+./benchmark_attn 8 16 256 64
+./benchmark_attn 16 8 256 64
