ade8505ae2aadc7ab7e36b73e8dcbf1bde04b23b
Dolan Clahan <dolan.clahan@complex.com>
2025-11-17 17:27:23 -0500

    do both

--- src/tests/profile-tests.sh
+++ src/tests/profile-tests.sh
@@ -1,3 +1,4 @@
 # run nsight or nsys profiler for best speedup configs, and normal regular config
 
+# everyone's best speedup was B=32 nh=16 N=64 d=64
 ncu --kernel-id ::flash_attn_forward_kernel:4 ./benchmark_attn 32 16 64 64 1
